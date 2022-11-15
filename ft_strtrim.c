16168ea79b14b9312f2ecfd2fd096b5d8ec87696
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2022-11-15 15:23:55 +0100

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 14:17:06 by cpalusze          #+#    #+#             */
-/*   Updated: 2022/11/14 08:55:57 by cpalusze         ###   ########.fr       */
+/*   Updated: 2022/11/15 15:23:27 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ static size_t	get_end(char const *s1, char const *set);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*trim;
 	size_t	start;
 	size_t	end;
 
@@ -27,8 +26,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(s1));
 	start = get_start(s1, set);
 	end = get_end(s1, set);
-	trim = ft_substr(s1, start, end - start + 1);
-	return (trim);
+	return (ft_substr(s1, start, end - start + 1));
 }
 
 static size_t	get_start(char const *s1, char const *set)
