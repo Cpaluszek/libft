5f98e28073d58c8f9eaf7a8761a41ee087b82fb2
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-01-06 12:44:26 +0100

    fix

--- src/mem/ft_free_split.c
+++ src/mem/ft_free_split.c
@@ -6,7 +6,7 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 12:41:19 by cpalusze          #+#    #+#             */
-/*   Updated: 2023/01/06 12:42:41 by cpalusze         ###   ########.fr       */
+/*   Updated: 2023/01/06 12:44:17 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ void	ft_free_split(char **strs)
 
 	i = 0;
 	while (strs[i])
-		free(strs[i]);
+		free(strs[i++]);
 	free(strs);
 }
