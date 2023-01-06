cc8e18842f4994c575e24a9462b8b8e6007979a6
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-01-06 12:46:27 +0100

    add ft_free_split

--- inc/libft.h
+++ inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: cpalusze <cpalusze@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:23:15 by cpalusze          #+#    #+#             */
-/*   Updated: 2022/12/09 15:17:52 by cpalusze         ###   ########.fr       */
+/*   Updated: 2023/01/06 12:46:06 by cpalusze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	ft_free(void *ptr);
+void	ft_free_split(char **strs);
 
 /* Input-Output */
 void	ft_putchar_fd(const char c, int fd);
